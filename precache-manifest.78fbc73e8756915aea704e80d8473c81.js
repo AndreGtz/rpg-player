17ee06484f3e6cc358d3e7e225988038fe3da967
andregtz <colpac64@gmail.com>
2018-10-13 01:10:27 -0500

    Updates

---
+++ precache-manifest.78fbc73e8756915aea704e80d8473c81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f22a393eeef9c37808d5",
+    "url": "/static/js/main.f22a393e.chunk.js"
+  },
+  {
+    "revision": "6c39ed5fb89d7778602c",
+    "url": "/static/js/1.6c39ed5f.chunk.js"
+  },
+  {
+    "revision": "f22a393eeef9c37808d5",
+    "url": "/static/css/main.b7068d44.chunk.css"
+  },
+  {
+    "revision": "43c605e09c84e76803aca650bdcf4f2e",
+    "url": "/index.html"
+  }
+];
